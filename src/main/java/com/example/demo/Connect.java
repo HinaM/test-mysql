a3ce0e01b2bfbf5998382009b3db7e3f060bd8dd
Ruby <Z00177@mpinfo.com.tw>
2023-04-07 16:28:31 +0800

    456

--- src/main/java/com/example/demo/Connect.java
+++ src/main/java/com/example/demo/Connect.java
@@ -1,3 +1,4 @@
+/*
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +14,81 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class Connect {
-	
-	   
+	    
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
 	@GetMapping("/get-users")
-	public List<Map<String, Object>> getUserList(String testName){
+	public int getUserList(String testName){
 		
-		String sql = "select * from user_account";
 		
+		String sql = "SELECT * FROM user_account";
 		List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql);
 		
-		return list;
+		int size=list.size();
+		return size;
 		
 		
 	}
 	
-	
 }
+*/
+package com.example.demo;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@RestController
+public class Connect {
+
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	@GetMapping("/index")
+	public String helloIndex(){
+		return "index";
+	}
+	
+	//添加SQL驗證帳號
+
+	@GetMapping("result")
+	public ModelAndView home(@RequestParam(required = false) String user_name,@RequestParam(required = false) String user_password) {
+		
+		String sql = "select * from user_account where user_name = ? and user_password = ?";
+		
+		List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql,new Object[] {user_name,user_password});
+		int size=list.size();
+		if (size > 0) {
+			
+			ModelAndView model = new ModelAndView("hello");
+			model.addObject("name", user_name);
+			model.addObject("psd", user_password);
+			return model;
+			
+			
+		}else {
+			
+			ModelAndView model = new ModelAndView("error");
+			model.addObject("num", size);
+			return model;
+		
+		}
+		
+		
+	}
+}
